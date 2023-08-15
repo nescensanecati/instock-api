3f2146b146ec366e8f2c5597dd1c46670ba6a6c0
Jay Rodriguez <74851511+Jairo1031@users.noreply.github.com>
2023-08-15 16:53:35 -0400

    created the database connection add update the variables to the provided names

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Import dotenv to process environment variables from `.env` file.
+require("dotenv").config();
+
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_LOCAL_DBNAME,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
+    charset: "utf8",
+  },
+};
