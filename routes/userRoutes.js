89798d35733dda029b9c994c52315159a26108fb
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-17 11:08:39 -0400

    adding api functionality to get list of all inventory items

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const inventoriesDelete = require("../controllers/inventoriesDelete"); // Import your controller module
+const inventoriesDelete = require("../controllers/inventoriesDelete"); // Deletion controller module
+
+const inventoriesGet = require("../controllers/inventoriesGet"); // Get "all" controller module
+
+// GET "all" route for getting all inventory items
+router.get("/", inventoriesGet.get);
 
 // DELETE route for deleting an inventory item by ID
-
 router.delete("/:id", inventoriesDelete.remove);
+
 
 module.exports = router;
