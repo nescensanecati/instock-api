d130feac07bc022d111aca70d0ce66df2e22ce2d
Alejandro Malagon Diaz <amd@outlook.es>
2023-08-17 16:43:23 -0400

    adding a put route to inventories to update an inventory item

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const inventoryRoute = express.Router();
 
+inventoryRoute.use(express.json())
+
+
 const inventoriesDelete = require("../controllers/inventoriesDelete"); // inventories DELETE controller module
 
 const inventoriesGet = require("../controllers/inventoriesGet"); // inventories GET controller module
 
+const inventoriesPut = require("../controllers/inventoriesPut"); // inventories PUT controller module
+
+
 // inventories GET route for getting all inventory items
 inventoryRoute.get("/", inventoriesGet.getAll);
 
-// inventories GET route for getting all inventory items
+// inventories GET route for getting single inventory item
 inventoryRoute.get("/:id", inventoriesGet.getSingleItem);
+
+// inventories PUT route for updating a single inventory item
+inventoryRoute.put("/:id", inventoriesPut.putSingleItem);
 
 // inventories DELETE route for deleting an inventory item by ID
 inventoryRoute.delete("/:id", inventoriesDelete.remove);
