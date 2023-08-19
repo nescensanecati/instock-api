480eb65b6b5815a42d192ccdaa1335551ba78fee
Jay Rodriguez <74851511+Jairo1031@users.noreply.github.com>
2023-08-18 22:29:41 -0400

    added the database connection

---
+++ controllers/warehouseDelete.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const knex = require('knex') (require("../knexfile"));
+const fs = require("fs");
+
+const remove = (req, res) => {
+    knex()
+}
+
